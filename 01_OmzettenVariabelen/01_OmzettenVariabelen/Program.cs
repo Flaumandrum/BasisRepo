c7f691a0535d4a4e5eb8853c263ba4f877521639
Tom Adriaens <tomadriaens@hotmail.com>
2022-09-16 08:53:04 +0200

    [LES] stand. comm. toev.

---
+++ 01_OmzettenVariabelen/01_OmzettenVariabelen/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_OmzettenVariabelen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Tom Adriaens
+             * 16/09/2022
+             * Omzetten Variabelen
+             */
+
+            // Velden 
+
+            // Programma
+
+
+        }
+    }
+}
