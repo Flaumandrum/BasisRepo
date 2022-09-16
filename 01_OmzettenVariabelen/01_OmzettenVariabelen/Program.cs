36d4bf330c3d9e8d3c3646796058056c00ae53cd
Tom Adriaens <tomadriaens@hotmail.com>
2022-09-16 09:16:34 +0200

    [LES] analyse Toegevoegd

--- 01_OmzettenVariabelen/01_OmzettenVariabelen/Program.cs
+++ 01_OmzettenVariabelen/01_OmzettenVariabelen/Program.cs
@@ -20,6 +20,16 @@ namespace _01_OmzettenVariabelen
 
             // Programma
 
+            // Stap 1:  Intro
+
+            //Stap 2: Vraag getal 1 + opslaan
+
+            //Stap 3: Vraag getal 2 + opslaan
+
+            //Stap 4: Vraag getal 3 + opslaan
+
+            //Stap 5: Toon de getallen in omgekeerde volgorde
+
 
         }
     }
