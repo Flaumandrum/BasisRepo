05009dbb1f033b9224cda0b24e8b1f38b737bd52
Tom Adriaens <tomadriaens@hotmail.com>
2022-09-20 11:29:49 +0200

    [LES] TryCatsch toegevoegd

--- 01_OmzettenVariabelen/01_OmzettenVariabelen/Program.cs
+++ 01_OmzettenVariabelen/01_OmzettenVariabelen/Program.cs
@@ -28,41 +28,53 @@ namespace _01_OmzettenVariabelen
             //Scherm leegmaken 
             Console.Clear();
 
-            // Stap 2: Vraag getal 1 + opslaan
-            Console.Write("Geef getal 1 in: ");
-            _getal1 = Int32.Parse( Console.ReadLine());
-            Console.WriteLine();
-            Console.WriteLine();
+            //fout afhandeling
+            try
+            {
+                // Stap 2: Vraag getal 1 + opslaan
+                Console.Write("Geef getal 1 in: ");
+                _getal1 = Int32.Parse(Console.ReadLine());
+                Console.WriteLine();
+                Console.WriteLine();
 
-            // Stap 3: Vraag getal 2 + opslaan
-            Console.Write("Geef getal 2 in: ");
-            _getal2 = Int32.Parse(Console.ReadLine());
-            Console.WriteLine();
-            Console.WriteLine();
+                // Stap 3: Vraag getal 2 + opslaan
+                Console.Write("Geef getal 2 in: ");
+                _getal2 = Int32.Parse(Console.ReadLine());
+                Console.WriteLine();
+                Console.WriteLine();
 
-            // Stap 4: Vraag getal 3 + opslaan
-            Console.Write("Geef getal 3 in: ");
-            _getal3 = Int32.Parse(Console.ReadLine());
-            Console.WriteLine();
-            Console.WriteLine();
+                // Stap 4: Vraag getal 3 + opslaan
+                Console.Write("Geef getal 3 in: ");
+                _getal3 = Int32.Parse(Console.ReadLine());
+                Console.WriteLine();
+                Console.WriteLine();
 
-            // Begeleid de gebruiker
-            Console.WriteLine("Bedankt om de getallen in te geven. \n\nDruk op enter om verder te gaan.");
-            Console.ReadKey();
+                // Begeleid de gebruiker
+                Console.WriteLine("Bedankt om de getallen in te geven. \n\nDruk op enter om verder te gaan.");
+                Console.ReadKey();
 
-            //Scherm leegmaken 
-            Console.Clear();
+                //Scherm leegmaken 
+                Console.Clear();
 
-            // Stap 5: Toon de getallen in omgekeerde volgorde
-            Console.WriteLine("Hier zijn jouw 3 getallen in omgekeerde volgorde:\n");
-            Console.WriteLine("Getal 3 : " + _getal3.ToString());
-            Console.WriteLine("Getal 2 : {0}", _getal2.ToString());
-            Console.WriteLine($"Getal 1 : {_getal1.ToString()} ");
+                // Stap 5: Toon de getallen in omgekeerde volgorde
+                Console.WriteLine("Hier zijn jouw 3 getallen in omgekeerde volgorde:\n");
+                Console.WriteLine("Getal 3 : " + _getal3.ToString());
+                Console.WriteLine("Getal 2 : {0}", _getal2.ToString());
+                Console.WriteLine($"Getal 1 : {_getal1.ToString()} ");
 
-            // Begeleid de gebruiker
-            Console.WriteLine(" \n\nDruk op enter om af te sluiten.");
-            Console.ReadKey();
+                // Begeleid de gebruiker
+                Console.WriteLine(" \n\nDruk op enter om af te sluiten.");
+                Console.ReadKey();
+            }
+            catch
+            {
+                //Scherm leegmaken 
+                Console.Clear();
 
+                // Begeleid de gebruiker
+                Console.WriteLine("U gaf geen juiste getalwaarden in.\n\nDruk op enter om af te sluiten.");
+                Console.ReadKey();
+            }
         }
     }
 }
