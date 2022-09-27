bf9e9efc15d628867ed6649662d18e90b20f8cb8
Tom Adriaens <tomadriaens@hotmail.com>
2022-09-27 11:27:47 +0200

    [Les] if functie voor test

---
+++ 03 If_functie/03 If_functie/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_If_functie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tom Adriaens
+            // Project if functie
+            // 27/09/2022
+
+            //Velden 
+            byte _keuze = 0;
+
+            //Programma
+            // Stap 1) intro
+            Console.WriteLine("Welkom!\n\nDit programma stelt je een heel belangrijke vraag!");
+            Console.WriteLine("\nDruk op enter om verder te gaan");
+            Console.ReadKey();
+
+            // Scherm leegmaken 
+            Console.Clear();
+
+            // Stap 2) Stel de vraag + Toon de keuzes(Pitta, Franse Hamburger, kapsalon, iets anders)
+            Console.WriteLine("Dit is de vraag: Wat bestel jij bij pitta Karine?");
+            Console.WriteLine("\nHier zijn jouw keuzes:");
+            Console.WriteLine("\n   1) Pitta\n   2) Franse Hamburger\n   3) Kapsalon\n   4) Iets anders");
+            try
+            {
+
+                // Stap 3) Vraag keuze +opslaan
+                Console.Write("\n\nGeef het nummer van uw keuze in: ");
+                _keuze = byte.Parse(Console.ReadLine());
+
+                // Scherm leegmaken 
+                Console.Clear();
+
+                // Stap 4) Toon de juiste tekst
+                //  Als 1: U koos Pitta
+                if (_keuze == 1)
+                {
+                    Console.WriteLine("U koos Pitta.");
+                }
+                //  Als 2: U koos Franse Hamburger
+                else if (_keuze == 2)
+                {
+                    Console.WriteLine("U koos Franse Hamburger.");
+                }
+                //  Als 3: U koos Kapsalon
+                else if (_keuze == 3)
+                {
+                    Console.WriteLine("U koos Kapsalon.");
+                }
+                //  Als 4: U koos iets anders
+                else 
+                {
+                    Console.WriteLine("U koos Iets Anders.");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("\nDruk op enter om af te sluiten.");
+                Console.ReadKey();
+
+
+            }
+            catch
+            {
+
+                // Scherm leegmaken 
+                Console.Clear();
+
+                Console.WriteLine("U gaf geen getal in. ");
+                Console.WriteLine("\nDruk op enter om af te sluiten.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ 03 If_functie/03 If_functie/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("03 If_functie")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Oscar Romerocollege")]
+[assembly: AssemblyProduct("03 If_functie")]
+[assembly: AssemblyCopyright("Copyright © Oscar Romerocollege 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a48237ee-9f44-47cc-93d9-e8809ea5fe14")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
