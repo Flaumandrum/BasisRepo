3161374c83324fbcdc0ec50b269e3d20ceca2b72
Tom Adriaens <tomadriaens@hotmail.com>
2022-09-27 10:29:01 +0200

    [Les] afgewerkt voor test

--- 02ProJ_Wisselgeld/02ProJ_Wisselgeld/Program.cs
+++ 02ProJ_Wisselgeld/02ProJ_Wisselgeld/Program.cs
@@ -17,7 +17,8 @@ namespace _02ProJ_Wisselgeld
             // Velden
             Decimal _tebetalen = 0, _ontvangen = 0, _wisselgeld;
             int _werkbedrag=0, _rest=0 ;
-
+            const int wis1 = 100, wis2 = 50, wis3 = 20, wis4 = 10, wis5 = 5, wis6 = 2, wis7= 1;
+            
             // Programma 
 
             // Stap 1) intro
@@ -37,15 +38,122 @@ namespace _02ProJ_Wisselgeld
                 // Stap 3) vraag ontvangen geldbedrag + opslaan
                 Console.Write("Geef het ontvangen bedrag in: ");
                 _ontvangen = decimal.Parse(Console.ReadLine());
+
+                Console.WriteLine("\nDruk op enter om het wisselgeld te bekijken.");
+                Console.ReadKey();
+
+                //Scherm leegmaken 
+                Console.Clear();
+
                 // Stap 4) bereken wisselgeld
                 _wisselgeld = _ontvangen - _tebetalen;
+                Console.WriteLine(_wisselgeld.ToString());
 
                 // a)	deel door waarde wisselgeld
+                //100
+                _werkbedrag = Convert.ToInt32(_wisselgeld);
+                _werkbedrag = Convert.ToInt32(Math.Floor(_wisselgeld / wis1));
                 // b)	mod deling door waarde wisselgeld
-                // c)	verminder van het totaal wisselgeld
-                // Stap 5) toon het resultaat
+                _wisselgeld = _wisselgeld % wis1;
+
+                Console.WriteLine($"{_werkbedrag.ToString()} briefjes van 100 ");
+                //50
+                _werkbedrag = Convert.ToInt32(_wisselgeld);
+                _werkbedrag = Convert.ToInt32(Math.Floor(_wisselgeld / wis2));
+                // b)	mod deling door waarde wisselgeld
+                _wisselgeld = _wisselgeld % wis2;
+
+                Console.WriteLine($"{_werkbedrag.ToString()} briefjes van 50 ");
+
+                // 20
+                _werkbedrag = Convert.ToInt32(_wisselgeld);
+                _werkbedrag = Convert.ToInt32(Math.Floor(_wisselgeld / wis3));
+                // b)	mod deling door waarde wisselgeld
+                _wisselgeld = _wisselgeld % wis3;
+
+                Console.WriteLine($"{_werkbedrag.ToString()} briefjes van 20 ");
+
+                // 10
+                _werkbedrag = Convert.ToInt32(_wisselgeld);
+                _werkbedrag = Convert.ToInt32(Math.Floor(_wisselgeld / wis4));
+                // b)	mod deling door waarde wisselgeld
+                _wisselgeld = _wisselgeld % wis4;
+
+                Console.WriteLine($"{_werkbedrag.ToString()} briefjes van 10 ");
+
+                // 5
+                _werkbedrag = Convert.ToInt32(_wisselgeld);
+                _werkbedrag = Convert.ToInt32(Math.Floor(_wisselgeld / wis5));
+                // b)	mod deling door waarde wisselgeld
+                _wisselgeld = _wisselgeld % wis5;
+
+                Console.WriteLine($"{_werkbedrag.ToString()} briefjes van 5 ");
 
 
+                //2
+                _werkbedrag = Convert.ToInt32(_wisselgeld);
+                _werkbedrag = Convert.ToInt32(Math.Floor(_wisselgeld / wis6));
+                // b)	mod deling door waarde wisselgeld
+                _wisselgeld = _wisselgeld % wis6;
+
+                Console.WriteLine($"{_werkbedrag.ToString()} stukken van 2 ");
+
+                // 1
+                _werkbedrag = Convert.ToInt32(_wisselgeld);
+                _werkbedrag = Convert.ToInt32(Math.Floor(_wisselgeld / wis7));
+                // b)	mod deling door waarde wisselgeld
+                _wisselgeld = _wisselgeld % wis7;
+
+                Console.WriteLine($"{_werkbedrag.ToString()} stukken van 1 ");
+
+                // 0.5
+                _wisselgeld = _wisselgeld * 100;
+                _werkbedrag = Convert.ToInt32(Math.Floor(_wisselgeld / wis2));
+                // b)	mod deling door waarde wisselgeld
+                _wisselgeld = _wisselgeld % wis2;
+
+                Console.WriteLine($"{_werkbedrag.ToString()} stukken van van 50 cent ");
+
+                // 0.2
+                _werkbedrag = Convert.ToInt32(Math.Floor(_wisselgeld / wis3));
+                // b)	mod deling door waarde wisselgeld
+                _wisselgeld = _wisselgeld % wis3;
+
+                Console.WriteLine($"{_werkbedrag.ToString()} stukken van van 20 cent ");
+                
+
+
+                // 0.1                
+                _werkbedrag = Convert.ToInt32(Math.Floor(_wisselgeld / wis4));
+                // b)	mod deling door waarde wisselgeld
+                _wisselgeld = _wisselgeld % wis4;
+
+                Console.WriteLine($"{_werkbedrag.ToString()} stukken van van 10 cent ");
+
+                // 0.05                
+                _werkbedrag = Convert.ToInt32(Math.Floor(_wisselgeld / wis5));
+                // b)	mod deling door waarde wisselgeld
+                _wisselgeld = _wisselgeld % wis5;
+
+                Console.WriteLine($"{_werkbedrag.ToString()} stukken van van 5 cent ");
+
+                // 0.02                
+                _werkbedrag = Convert.ToInt32(Math.Floor(_wisselgeld / wis6));
+                // b)	mod deling door waarde wisselgeld
+                _wisselgeld = _wisselgeld % wis6;
+
+                Console.WriteLine($"{_werkbedrag.ToString()} stukken van van 2 cent ");
+
+              
+
+                // 0.01                
+                _werkbedrag = Convert.ToInt32(Math.Floor(_wisselgeld / wis7));
+                // b)	mod deling door waarde wisselgeld
+                _wisselgeld = _wisselgeld % wis7;
+
+                Console.WriteLine($"{_werkbedrag.ToString()} stukken van van 1 cent ");
+
+                Console.ReadKey();
             }
             catch
             {
