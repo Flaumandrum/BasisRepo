6ca4e3763fda68e56733d9539addb41073a06b67
Tom Adriaens <tomadriaens@hotmail.com>
2023-01-27 09:22:51 +0100

    [Les] afgewerkt

--- ProjectAanwezigheden/ProjectAanwezigheden/Program.cs
+++ ProjectAanwezigheden/ProjectAanwezigheden/Program.cs
@@ -36,6 +36,7 @@ namespace ProjectAanwezigheden
             Console.Write("\nAantal leerlingen: ");
             _aantalLln = byte.Parse(Console.ReadLine());
 
+            // grootte arrays aanpassen
             _achternamen = new string[_aantalLln];
             _voornamen = new string[_aantalLln];
 
@@ -87,7 +88,7 @@ namespace ProjectAanwezigheden
                             Console.Write("Geef de voornaam:");
                             _voornamen[_gevonden] = Console.ReadLine();
                             Console.Write("\nGeef de achternaam:");
-                            _voornamen[_gevonden] = Console.ReadLine();
+                            _achternamen[_gevonden] = Console.ReadLine();
                         }
 
                         // Of toon foutmelding
@@ -113,16 +114,38 @@ namespace ProjectAanwezigheden
                         {
                             if (_voornamen[i] != null)
                             {
-                                Console.WriteLine($" {i + 1}) {_voornamen[i]} {_achternamen[i]}");
+                                Console.WriteLine($" {(i + 1).ToString()}) {_voornamen[i]} {_achternamen[i]}");
                             }
                             else
                             {
-                                Console.WriteLine($" {i + 1}) leeg ");
+                                Console.WriteLine($" {(i + 1).ToString()}) leeg ");
                             }
 
                         }
                         // vraag de nummer van de leerling om te veranderen
+                        Console.Write("\n\nGeef het nummer van de leerling die je zou willen aanpassen: ");
+                        _nummerLln= sbyte.Parse(Console.ReadLine());
+                        _nummerLln -= 1;
+
+                        // Scherm leegmaken 
+                        Console.Clear();
+
                         // vraag voornaam &achternaam + sla op in arrays op de juiste plaats
+                        Console.Write("Geef de voornaam: ");
+                        _voornamen[_nummerLln] = Console.ReadLine();
+                        Console.Write("\nGeef de achternaam: ");
+                        _achternamen[_nummerLln] = Console.ReadLine();
+
+
+                        // Scherm leegmaken 
+                        Console.Clear();
+
+                        // gebruike begeleiden
+                        Console.WriteLine("Deze naam werd aangepast.");
+                        Console.WriteLine("\nDruk op enter om verder te gaan.");
+                        Console.ReadKey();
+
+
                     }
 
                     // Als 3: verwijderen
@@ -134,16 +157,32 @@ namespace ProjectAanwezigheden
                         {
                             if (_voornamen[i] != null)
                             {
-                                Console.WriteLine($" {i + 1}) {_voornamen[i]} {_achternamen[i]}");
+                                Console.WriteLine($" {(i + 1).ToString()} {_voornamen[i]} {_achternamen[i]}");
                             }
                             else
                             {
-                                Console.WriteLine($" {i + 1}) leeg ");
+                                Console.WriteLine($" {(i + 1).ToString()}) leeg ");
                             }
 
                         }
                         // vraag de nummer van de leerling om te verwijderen +verwijder
+
+                        Console.Write("\n\nGeef het nummer van de leerling die je zou willen aanpassen: ");
+                        _nummerLln = sbyte.Parse(Console.ReadLine());
+                        _nummerLln -= 1;
+
                         // verwijder in de arrays op de juiste plaats
+                        _voornamen[_nummerLln] = null;
+                        _achternamen[_nummerLln] = null;
+
+
+                        // Scherm leegmaken 
+                        Console.Clear();
+
+                        // gebruike begeleiden
+                        Console.WriteLine("Deze naam werd aangepast.");
+                        Console.WriteLine("\nDruk op enter om verder te gaan.");
+                        Console.ReadKey();
                     }
                     // Als 4: tonen
                     else if (_keuze == 4)
@@ -154,11 +193,11 @@ namespace ProjectAanwezigheden
                         {
                             if (_voornamen[i] != null)
                             {
-                                Console.WriteLine($" {i + 1}) {_voornamen[i]} {_achternamen[i]}");
+                                Console.WriteLine($" {(i + 1).ToString()}) {_voornamen[i]} {_achternamen[i]}");
                             }
                             else
                             {
-                                Console.WriteLine($" {i + 1}) leeg ");
+                                Console.WriteLine($" {(i + 1).ToString()}) leeg ");
                             }
 
                         }
@@ -167,14 +206,47 @@ namespace ProjectAanwezigheden
                         Console.ReadKey();
                     }
                     // Als 5: Afsluiten
-                    else if (_keuze == 1)
+                    else if (_keuze == 5)
                     {
+                        // sorteer de array
+                        Array.Sort(_voornamen, _achternamen);
 
+                        // Toon de leerlingen,
+                        Console.WriteLine("De aanwezige leerlingen zijn:\n\n");
+                        for (int i = 0; i < _voornamen.Length; i++)
+                        {
+                            if (_voornamen[i] != null)
+                            {
+                                Console.WriteLine($" {(i + 1).ToString()}) {_voornamen[i]} {_achternamen[i]}");
+                            }
+                            else
+                            {
+                                Console.WriteLine($" {(i + 1).ToString()}) leeg ");
+                            }
+
+                        }
+
+                        Console.WriteLine("\n\nDruk op enter om verder te gaan.");
+                        Console.ReadKey();
+
+                        // Scherm leegmaken 
+                        Console.Clear();
+
+                        // foutmelding
+                        Console.WriteLine("Tot een volgende keer.");
+                        Console.WriteLine("\nDruk op enter om verder te gaan.");
+                        Console.ReadKey();
                     }
                     // in elk ander geval
                     else
                     {
+                        // Scherm leegmaken 
+                        Console.Clear();
 
+                        // foutmelding
+                        Console.WriteLine("U gaf geen juiste keuze in.");
+                        Console.WriteLine("\nDruk op enter om verder te gaan.");
+                        Console.ReadKey();
                     }
                 }
                 catch
